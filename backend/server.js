080ff9425ebf3335cebbcc4b41e0bb527d0a500d
mddanish222 <mdddanish854@gmail.com>
2026-05-03 23:49:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 //server.js
 
 const app = require("./app");
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
