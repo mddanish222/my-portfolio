e425c409e832705e9de71a53e4352d1aa7c41dc5
Danish <mdddanish854@gmail.com>
2026-05-05 20:41:39 +0530

    fix babel config for CI (esm to cjs)

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,6 @@
-export default {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }]
+  ]
 };
