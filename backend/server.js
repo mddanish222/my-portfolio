e92e64adb258dcb8da4a840894c76fbe979ddb86
mddanish222 <mdddanish854@gmail.com>
2026-07-05 01:07:13 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+//server.js
+require("dotenv").config();
+const app = require("./app");
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
